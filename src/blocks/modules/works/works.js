2f8ce864d2cf1aa17b91462ead3b3b851469094a
z3plin <serj.raguzin@yandex.ru>
2020-10-03 22:30:41 +0300

    update

--- src/blocks/modules/works/works.js
+++ src/blocks/modules/works/works.js
@@ -1,14 +1,22 @@
-import Swiper, { Pagination, Autoplay } from 'swiper';
+import Swiper, { Pagination, Navigation, Autoplay } from 'swiper';
 
-Swiper.use([Pagination, Autoplay]);
+Swiper.use([Pagination, Navigation, Autoplay]);
 
 let worksSlider = new Swiper('.worksSlider-container', {
     loop: true,
+    slidesPerView: 4,
+    slidesPerGroup: 3,
+    loopFillGroupWithBlank: true,
+    spaceBetween: 30,
     autoplay: {
-        delay: 3000,
+        delay: 5000,
     },
     pagination: {
-      el: '.swiper-pagination',
-      dynamicBullets: true,
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.works-button-next',
+        prevEl: '.works-button-prev',
     },
 });
